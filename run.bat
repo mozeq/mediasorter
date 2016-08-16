d73c5aaca8d3e605d39d0c397ae4e70460d30fe8
Jiri Moskovcak <moskovcak@gmail.com>
2016-08-16 21:42:23 +0200

    add windows helper script for utorrent

---
+++ run.bat
@@ -0,0 +1,7 @@
+chdir /D E:\mediasorter
+echo "%~1" "%~2" >> debug.log
+Set _downloadPath=%~1
+
+IF "%_downloadPath:~-1%" == "\" (Set _downloadPath=%_downloadPath%\)
+
+"C:\Program Files\java\jdk1.7.0_45\jre\bin\java.exe" -jar "target/mediasorter-ng-jar-with-dependencies.jar" "%_downloadPath%" "D:\Media" %2 >> mediasort.log
